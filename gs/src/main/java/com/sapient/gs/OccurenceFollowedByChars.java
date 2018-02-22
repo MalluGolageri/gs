283e9d7768d47d0c69c2bc3a8364363678d2df6e
mallu <mallu@Mallus-MacBook-Pro.local>
2018-02-22 14:53:01 +0530

    updating gs solutions

--- gs/src/main/java/com/sapient/gs/OccurenceFollowedByChars.java
+++ gs/src/main/java/com/sapient/gs/OccurenceFollowedByChars.java
@@ -7,13 +7,13 @@ import java.util.Map;
 public class OccurenceFollowedByChars {
 	public static void main(String[] args) {
 		String s = "SSSSSTTPPQGUG";
-		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
-		for (int i = 0; i < s.length(); i++) {
-			char key=s.charAt(i);
-			map.put(key, map.containsKey(key) ? map.get(key) + 1 : 1);
-		}		
-		map.forEach((key,value)->{System.out.print(""+value+key);});
-		System.out.println();
+//		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+//		for (int i = 0; i < s.length(); i++) {
+//			char key=s.charAt(i);
+//			map.put(key, map.containsKey(key) ? map.get(key) + 1 : 1);
+//		}		
+//		map.forEach((key,value)->{System.out.print(""+value+key);});
+//		System.out.println();
 		countChars(s);
 	}
 	
